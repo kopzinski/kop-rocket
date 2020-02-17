3c2875fb3c0c582c9873dbad53be573a31db295a
mellop <Paulo.Mello@tabcorp.com.au>
2020-02-17 11:40:09 -0300

    feat: basic static definitions

---
+++ src/Definitions.js
@@ -0,0 +1,16 @@
+const Definitions = () => {
+    return {
+        userIndexDescriptor: 0,
+        userIdIndex: 0,
+        userCountryIndex: 1,
+        taskUserIdIndex: 1,
+        taskTimeSpentIndex: 2,
+        spacer: ' ',
+        floatStartStartValue: 0.0,
+        averageRoundFractionDigits: 2,
+        encoding: 'utf8',
+        lineSeparator: '\n'
+    }
+}
+
+export default Definitions
