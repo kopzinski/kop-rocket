053bd31e3716258e7b49d50d198f40e197870340
mellop <mellop@dbserver.com.br>
2020-03-25 08:20:21 -0300

    logic removed for study porposes

--- src/ExtractData.js
+++ src/ExtractData.js
@@ -8,25 +8,12 @@ const ExtractData = ({ definitions, readFile, getSourceFile }) => {
         taskTimeSpentIndex
     } = definitions
 
-    const lineValidation = lineAsArray => lineAsArray.length && !!lineAsArray[0]
-
     return async () => {
         return readFile(getSourceFile()).then(lines => {
-            const numberOfUsers = parseInt(lines[userIndexDescriptor], 10)
-            const tasksIndexDescriptor = numberOfUsers + 1
-            return lines.reduce((compiledData, lineAsString, lineIndex) => {
-                const lineAsArray = lineAsString.split(spacer)
+            
+            return lines.reduce((compiledData, lineAsString, lineIndex) => {             
 
-                if (lineValidation(lineAsArray)) {
-                    if ( lineIndex <= numberOfUsers && lineIndex > 0) {
-                        compiledData[lineAsArray[userIdIndex]] = { country: lineAsArray[userCountryIndex], times: [] }
-                    }
-                    if ( lineIndex > tasksIndexDescriptor) {
-                        compiledData[lineAsArray[taskUserIdIndex]].times.push(parseFloat(lineAsArray[taskTimeSpentIndex]))
-                    }
-                }
-
-                return compiledData
+                return "something"
             }, {})
         })
     }
--- src/GetTimesAverage.js
+++ src/GetTimesAverage.js
@@ -1,9 +1,7 @@
 const GetTimesAverage = ({ definitions }) => {
     const { floatStartStartValue, averageRoundFractionDigits } = definitions
     return (times) => {
-        const sum = times.reduce((a, b) => a + b, floatStartStartValue);
-        const avg = (sum / times.length) || floatStartStartValue
-        return avg.toFixed(averageRoundFractionDigits)
+        return "something"
     }
 }
 
